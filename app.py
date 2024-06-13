218031c2ecca0e9691eaa9b145b50170b6c42920
Hilona <kondrashka04@gmail.com>
2024-06-13 21:32:18 +0300

    Handle run method & made sign up method &
    add storages &
    add settings &
    add user instance &
    add user manager

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from auth_system.authentication_service import AuthSystem
-from registration_system.regisration_service import RegistrationSystem
+from registration_system.registration_service import RegistrationSystem
 
 
 class App:
     MENU = {
         "login": AuthSystem.auth,
-        "registr": RegistrationSystem.sign_up,
+        "register": RegistrationSystem.sign_up,
+        "exit": None,
     }
 
     def run(self):
-        command = input("Your choice please (login | registr)")
-        do = self.MENU[command]
-        do()
+        command = input("Your choice please (login | register | exit)")
+        try:
+            do = self.MENU[command]
+            do()
+        except KeyError:
+            print("Unknown command try again next time")
 
 
 app = App()
